a55d4cdbcb7428dff626ab6d610fd40aa7678f9e
Mihir-Ramjathan <ST10527496@myemeris.edu.za>
2026-03-09 08:35:22 +0200

    Declarations
    declaring Your variables

--- Logical Operator/app/src/main/java/com/example/logicalop/MainActivity.kt
+++ Logical Operator/app/src/main/java/com/example/logicalop/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.logicalop
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Declarations 
+    private lateinit var edtName: EditText
+    private lateinit var edtAge: EditText
+    private lateinit var btnCheck: Button
+    private lateinit var cbStaff: CheckBox
+    private lateinit var cbITStudent: CheckBox
+    private lateinit var cbBanned: CheckBox
+    private lateinit var textView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
